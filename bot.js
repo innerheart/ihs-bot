63193401e5cab3e3fd6c7b703af5af5df7ca45f6
innerheart <40139901+innerheart@users.noreply.github.com>
2018-06-10 17:54:54 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NDU0OTY5OTYzMjMzNTQyMTY0.Df6U2w.PABgD0Gwff1XwyhQCCA9-dSVdQY);
