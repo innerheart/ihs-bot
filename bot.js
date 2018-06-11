6e98ea731b093752cd7abcedd04e478f837b0a69
innerheart <40139901+innerheart@users.noreply.github.com>
2018-06-12 01:03:39 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ client.on('message', message => {
     if (message.content === '!ping') {
     	message.reply('pong');
   	}
+client.on('message', message => {
+    if (message.content === '!random') {
+    	message.reply('Math.floor(Math.random() * 100) + 1;');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
