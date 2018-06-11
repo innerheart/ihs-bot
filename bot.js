5df9edc909353cf46a6411d97447d7b22ce98ea6
innerheart <40139901+innerheart@users.noreply.github.com>
2018-06-12 01:26:53 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-import java.util.Random;
-
-Random rand = new Random();
-int  n = rand.nextInt(100) + 1;
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -13,10 +9,11 @@ client.on('message', message => {
     if (message.content === '!ping') {
     	message.reply('pong');
   	}
+
 client.on('message', message => {
-    if (message.content === '!random') {
-    	message.reply(n);
-  	}
+  if (message.content === '!avatar') {
+    message.reply(message.author.avatarURL);
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
