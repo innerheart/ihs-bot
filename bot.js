ce90bbeb8400171e1278d396d720beede796a453
innerheart <40139901+innerheart@users.noreply.github.com>
2018-06-19 21:07:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', message => {
     const command = args.shift().toLowerCase();
     
   if (command === 'avatar') {
-    let user = message.mentions.users.first() || message.author;
+    let user = message.mentions.users.first()
     let embed = new Discord.RichEmbed()
     .setAuthor(`${user.username}`)
     .setImage(user.displayAvatarURL)
