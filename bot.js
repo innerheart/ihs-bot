489a829331bc35cb7b8683e090e03d6e86f5c84b
innerheart <40139901+innerheart@users.noreply.github.com>
2018-06-19 22:38:07 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,54 @@ client.on('message', message => {
         message.channel.send('my master!');
     }
     
+    if(command === 'husbando'){
+        message.channel.send('https://pbs.twimg.com/media/DfOB3DqWkAE83ZJ.jpg:large');
+    }
+    
+    if(command === 'poker'){
+        message.channel.send({files:["https://i.imgur.com/mQR1csw.png"]});
+    }
+    
+    if(command === 'snek'){
+        message.channel.send("It's me, Snek!",{files:["https://i.imgur.com/JzYm9q7.png"]});
+    }
+    
+    if(command === 'gbfs'){
+        message.channel.send({files:["https://i.imgur.com/6tLrau1.png"]});
+    }
+    
+    if(command === 'bea'){
+        message.channel.send({files:["https://i.imgur.com/QkFvO2v.png"]});
+    }
+    
+    if(command === 'hitme'){
+        message.channel.send("Hit me!!",{files:["https://i.imgur.com/TVpXU3V.png"]});
+    }
+    
+    if(command === 'scary'){
+        message.channel.send({files:["https://i.imgur.com/CQPF2tc.gif"]});
+    }
+    
+    if(command === 'wow'){
+        message.channel.send("☆ **WOW !** ☆",{files:["https://gbf.wiki/images/0/06/Stamp57jp.png"]});
+    }
+    
+    if(command === 'teehee'){
+        message.channel.send("Tee-Hee〜★",{files:["https://gbf.wiki/images/8/8f/Stamp142jp.png"]});
+    }
+    
+    if(command === 'nani'){
+        message.channel.send("N.. Nani ?",{files:["https://i.imgur.com/3gRthTt.jpg"]});
+    }
+    
+     if(command === 'huh'){
+        message.channel.send("Huh?",{files:["https://cdn.discordapp.com/attachments/168749859111698432/457664238446444546/C6.png"]});
+    }
+    
+    if(command === 'zoi'){
+        message.channel.send({files:["http://i.imgur.com/yJ35t3i.png"]});
+    }
+    
     if (command === 'avatar') {
     let user = message.mentions.users.first() || message.author;
     let embed = new Discord.RichEmbed()
@@ -28,78 +76,6 @@ client.on('message', message => {
     .setImage(user.displayAvatarURL)
     .setColor('RANDOM')
     message.channel.send(embed)
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!husbando') {
-    message.channel.send("https://pbs.twimg.com/media/DfOB3DqWkAE83ZJ.jpg:large");
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!poker') {
-    message.channel.send( {files:["https://i.imgur.com/mQR1csw.png"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!snek') {
-    message.channel.send("It's me, Snek!",{files:["https://i.imgur.com/JzYm9q7.png"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!gbfs') {
-    message.channel.send( {files:["https://i.imgur.com/6tLrau1.png"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!bea') {
-    message.channel.send( {files:["https://i.imgur.com/QkFvO2v.png"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!hitme') {
-    message.channel.send("Hit me!!",{files:["https://i.imgur.com/TVpXU3V.png"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!scary') {
-    message.channel.send({files:["https://i.imgur.com/CQPF2tc.gif"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!wow') {
-    message.channel.send("☆ **WOW !** ☆",{files:["https://gbf.wiki/images/0/06/Stamp57jp.png"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!teehee') {
-    message.channel.send("Tee-Hee〜★",{files:["https://gbf.wiki/images/8/8f/Stamp142jp.png"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!nani') {
-    message.channel.send("N.. Nani !?",{files:["https://i.imgur.com/3gRthTt.jpg"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!huh') {
-    message.channel.send("Huh?",{files:["https://cdn.discordapp.com/attachments/168749859111698432/457664238446444546/C6.png"]});
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!zoi') {
-    message.channel.send({files:["http://i.imgur.com/yJ35t3i.png"]});
   }
 });
 
