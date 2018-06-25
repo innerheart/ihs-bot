c9756031cf63603b7dcb56dbc79775f7365f99df
innerheart <40139901+innerheart@users.noreply.github.com>
2018-06-25 13:04:26 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config.json");
 const prefix = '!';
 
 client.on('ready', () => {
@@ -19,12 +20,8 @@ client.on('message', message => {
     if(command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
+    let m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    }
-    
-    if(command === 'ihs.'){
-        message.channel.send('my master!');
     }
     
     if(command === 'husbando'){
