9e968560168d0c6ade26d8925c43b35602a29d72
innerheart <40139901+innerheart@users.noreply.github.com>
2019-09-14 09:36:14 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,7 @@ const activities_list = ['Type !help', 'with roulette',];
 
 client.on('ready', () => {
     console.log("I am ready!");
-    client.user.setGame("Type !help");
-    /*setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.*/
-
+    
 });
 client.on('message', message => {
    // var hlp = "There is something about `!emp` **?**";
