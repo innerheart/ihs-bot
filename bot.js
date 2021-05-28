f8d1b1e6f38c93eacc152e5de56afc29b755ddf5
ihs <40139901+ihs-git@users.noreply.github.com>
2021-05-28 19:38:01 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,8 +78,7 @@ client.on('message', message => {
     
     if(command === 'choose'){
     let pick = message.content.slice(prefix.length + 5).trim().split(',');
-    message.channel.send( `${pick[Math.floor(Math.random()* pick.length)] }\ `);
-         
+    message.channel.send('I pick ' + `${pick[Math.floor(Math.random()* pick.length)] }\ `);    
     }
     
     if(command === "say") {
