f316510fa25ec7b1106de578b417d0314d526db1
ihs <40139901+ihs-git@users.noreply.github.com>
2021-05-28 18:43:10 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '!';
-const activities_list = ['Type !help', 'with roulette',];
+const activities_list = ['Hello',];
 
 client.on('ready', () => {
     console.log("I am ready!");
